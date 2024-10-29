a06a6a706ede27e1938d6f698ddf4911914ffa2e
Darya <daria.lazareva@raketa.agency>
2024-10-29 13:03:23 +0300

    Updates

---
+++ _nuxt/DZsLRr9F.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,A as s,o as c}from"./BLymxDBo.js";const n={},a={class:"container"};function r(e,_){return c(),o("div",a,[s(e.$slots,"default",{},void 0)])}const f=t(n,[["render",r],["__scopeId","data-v-2e156213"]]);export{f as default};
