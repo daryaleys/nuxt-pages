e46809a1504f52b1b71d41f0a2b631b0f1ba7405
Darya <daria.lazareva@raketa.agency>
2024-10-29 13:07:31 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,5 @@ export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
 	devtools: { enabled: true },
 	css: ["~/assets/style/main.scss"],
-	router: {
-		base: "/nuxt-pages/",
-	},
+	base: "/nuxt-pages/",
 });
